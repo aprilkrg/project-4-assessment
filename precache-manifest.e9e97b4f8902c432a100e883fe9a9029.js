6fb26d93f761fb54885d1c29198e7fa0b4e9d680
April <macuser@Mac-User.local>
2020-07-17 14:59:58 -0600

    Updates

---
+++ precache-manifest.e9e97b4f8902c432a100e883fe9a9029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b790a8610df2271d0d45b1243a74b3",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "ff4cfb6c6b35ffc96d71",
+    "url": "/project-4-assessment/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4cfb6c6b35ffc96d71",
+    "url": "/project-4-assessment/static/js/main.669086ce.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
